84842df8d56394b11a25fb0e11e78a0e63c4c9d2
Vanshul Choudhary <75979288+Cvanshulll@users.noreply.github.com>
2021-11-10 23:16:33 +0530

    Create 59A.cpp

---
+++ codeforces/59A.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void solve()
+{
+//  int k;
+//  cin>>k;
+//  set<int> s; // set 
+//  for(int i=k+1;i<=9050;i++){ // increase year by 1 
+//    int n=i;
+//    while(n){
+//      int last=n%10;
+//      s.insert(last);
+//      n=n/10;
+//    }
+//    if(s.size()==4){
+//      cout<<i;
+//      break;
+//    }
+//    else{
+//      s.clear();
+//    }
+//  }
+// 
+//}
+// 
+ 
+	string s ;
+	int u=0, l=0;
+	cin>>s;
+	for(int i=0; i<s.size(); i++)
+	{
+		if(s[i]>='A' && s[i]<='Z')
+		{
+			u++;
+		}
+		else
+		{
+			l++;
+		}
+	}
+	if(u<l)
+	{
+		transform(s.begin(), s.end(), s.begin(), ::tolower);
+//		tolower(s);
+		cout<<s<<endl;
+	}
+	else if(u==l)
+	{
+			transform(s.begin(), s.end(), s.begin(), ::tolower);
+//		tolower(s);
+		cout<<s<<endl;
+	}
+	else{
+		transform(s.begin(), s.end(), s.begin(), ::toupper);
+//		toupper(s);
+		cout<<s;
+	}
+}
+int main()
+{
+  
+      solve();
+    
+    return 0;
+}
