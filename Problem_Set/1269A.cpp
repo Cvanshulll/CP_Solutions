72a86bbcdf6dd61c451f4f40f4cae85442e58cc4
Vanshul Choudhary <75979288+Cvanshulll@users.noreply.github.com>
2022-01-26 23:45:40 +0530

    Create 1269A.cpp

---
+++ Problem_Set/1269A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std; 
+typedef long long ll;
+
+
+
+int main(){
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    
+
+    
+        // ll t;
+        // cin>>t;
+        // while(t--)
+        // {
+          
+
+        // }
+        
+        ll n;
+        cin>>n;
+        cout<<9*n<<" "<<8*n<<"\n";
+        return 0;
+
+}
+    
