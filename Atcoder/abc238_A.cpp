ea613cb006a8a38b5560d1f2be00ae106ba74bcf
Vanshul Choudhary <75979288+Cvanshulll@users.noreply.github.com>
2022-02-06 13:39:57 +0530

    upsolved

---
+++ Atcoder/abc238_A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std; 
+typedef long long ll;
+
+
+// #define m =10e
+int main(){
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+
+  ll n ;
+  cin>>n;
+
+  if(2<=n && n<=4)
+  {
+    cout<<"No"<<endl;
+  }
+  else
+  {
+    cout<<"Yes"<<endl;
+  }
+
+  return 0;
+}
+
