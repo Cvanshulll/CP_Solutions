55e9a3060d25640c917bd24f33d7b8dd248ea5e1
Vanshul Choudhary <75979288+Cvanshulll@users.noreply.github.com>
2022-02-27 23:27:20 +0530

    Create 677A.cpp

---
+++ Ladder/677A.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std; 
+typedef long long ll;
+#define all(v) begin(v), end(v)
+ 
+ 
+ 
+  // cin.getline(s,len);
+  // getline(cin,s);
+ 
+ 
+void solve ()
+{
+  ll n , h;
+  cin>>n>>h;
+  ll a[n];
+  ll c=0;
+  for(ll i =0;i<n ; i++)
+    {
+      cin>>a[i];
+      if(a[i]<=h)
+      {
+        c+=1;
+      }
+      else 
+      {
+        c+=2;
+      }
+    }
+  cout<<c<<endl;
+}
+ 
+// 10
+// 9 6 1 8 3 9 7 5 10 4
+// 7 1 3 2 7 6 9 5
+ 
+ 
+// #define m =10e
+int main()
+{
+ 
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr); cout.tie(nullptr);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+  // const unsigned int M = 1000000007;
+  
+ 
+  // ll t;
+  // cin>>t;
+  // while(t--)
+  // {
+    solve();
+  // }
+  
+  return 0;
+}
